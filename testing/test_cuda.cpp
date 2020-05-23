d078a3646a645cfd00d2f0ea979a147a7639d1f0
Yeo Jin Yan <jyyeo@caltech.edu>
2020-05-22 20:22:10 -0700

    fix

--- testing/test_cuda.cpp
+++ testing/test_cuda.cpp
@@ -6,19 +6,16 @@
 int main(int argc, char const *argv[])
 {
 	float arr1[10];
-        float arr2[10];
-        float arr3[10];
+    float arr2[10];
+    float arr3[10];
+    
 	for (int i = 0; i < 10; i++) {
 		arr1[i] = i + 0.5;
 		arr2[i] = i - 0.5;
 		arr3[i] = 0.0;
 		printf("%f\n", arr1[i]+arr2[i]);
 	}
-	cudaCallAddVecKernel(const int 1,
-	const unsigned int 32,
-	const float arr1,
-	const float arr2,
-	float arr3,
-	const unsigned int 32) 
+	cudaCallAddVecKernel(1, 32, arr1, arr2, arr3, 32);
+
 	return 0;
 }
