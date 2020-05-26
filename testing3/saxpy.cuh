b15ee585b14030be524dcacebd3f3048eeffba65
Yeo Jin Yan <jyyeo@titan.caltech.edu>
2020-05-25 19:27:09 -0700

    testing3 works

--- testing3/saxpy.cuh
+++ testing3/saxpy.cuh
@@ -1,5 +1,5 @@
 #pragma once
 
-void saxpy (float a, float *x, float *y, int size);
+__global__ void saxpy (float a, float *x, float *y, int size);
 
 void cudaSaxpy (float a, float *x, float *y, int size);
