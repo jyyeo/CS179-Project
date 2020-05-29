ba21227e97be28767687195b21c754b2114ea8e5
Yeo Jin Yan <jyyeo@caltech.edu>
2020-05-28 19:46:56 -0700

    tidy up testing4

--- testing4/findMax.cuh
+++ testing4/findMax.cuh
@@ -1,6 +1,6 @@
 #ifndef CUDA_FINDMAX_CUH
 #define CUDA_FINDMAX_CUH
 
-void cudaFindMax(float *dev_arr, int size, float *dev_max_val);
+void cudaFindMax(float *arr, int size, float *max_val);
 
 #endif
