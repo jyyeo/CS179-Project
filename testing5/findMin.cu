137c81cbb7bfb216a809b8f9a4e4813d3308c5c4
Yeo Jin Yan <jyyeo@caltech.edu>
2020-05-28 21:15:51 -0700

    findMin

--- testing5/findMin.cu
+++ testing5/findMin.cu
@@ -10,7 +10,7 @@ __device__ static float atomicMin(float* address, float val) {
     do {
         assumed = old;
         old = ::atomicCAS(address_as_i, assumed,
-            __float_as_int(::fmaxf(val, __int_as_float(assumed))));
+            __float_as_int(::fminf(val, __int_as_float(assumed))));
     } while (assumed != old);
     return __int_as_float(old);
 }
