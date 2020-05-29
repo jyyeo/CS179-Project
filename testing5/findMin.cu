09bc25f237d46ddbb35eed0204a61894aab9a92d
Yeo Jin Yan <jyyeo@caltech.edu>
2020-05-28 21:20:32 -0700

    findMin

--- testing5/findMin.cu
+++ testing5/findMin.cu
@@ -4,15 +4,23 @@
 #include "cuda_runtime.h"
 #include "findMin.cuh"
 
-__device__ static float atomicMin(float* address, float val) {
-    int* address_as_i = (int*) address;
-    int old = *address_as_i, assumed;
-    do {
-        assumed = old;
-        old = ::atomicCAS(address_as_i, assumed,
-            __float_as_int(::fminf(val, __int_as_float(assumed))));
-    } while (assumed != old);
-    return __int_as_float(old);
+// __device__ static float atomicMin(float* address, float val) {
+//     int* address_as_i = (int*) address;
+//     int old = *address_as_i, assumed;
+//     do {
+//         assumed = old;
+//         old = ::atomicCAS(address_as_i, assumed,
+//             __float_as_int(::fminf(val, __int_as_float(assumed))));
+//     } while (assumed != old);
+//     return __int_as_float(old);
+// }
+
+__device__ __forceinline__ float atomicMin (float * addr, float value) {
+        float old;
+        old = (value >= 0) ? __int_as_float(atomicMin((int *)addr, __float_as_int(value))) :
+             __uint_as_float(atomicMax((unsigned int *)addr, __float_as_uint(value)));
+
+        return old;
 }
 
 __global__ void findMin(float *dev_arr, int size, float *dev_min_val) {
