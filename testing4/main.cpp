e2e43291d84b962dd3fe4b4ba3d70e4585cd010d
Yeo Jin Yan <jyyeo@caltech.edu>
2020-05-28 19:51:56 -0700

    findMax completed

--- testing4/main.cpp
+++ testing4/main.cpp
@@ -17,21 +17,8 @@ int main(void) {
 		printf("%f ", arr[i]);
 	}
 	printf("\n");
- 	
- // 	float *dev_arr;
-	// float *dev_max_val;
 
-	// cudaMalloc((void**)&dev_arr, size * sizeof(float));
-	// cudaMalloc((void**)&dev_max_val, sizeof(float));
-
-	// cudaMemcpy(dev_arr, arr, size * sizeof(float), cudaMemcpyHostToDevice);
-	// cudaMemset(dev_max_val, 0.0, sizeof(float));
-
- // 	cudaFindMax(dev_arr, size, dev_max_val);
  	cudaFindMax(arr, size, max_val);
-
- // 	cudaMemcpy(&max_val, dev_max_val, sizeof(float), cudaMemcpyDeviceToHost);	
- // 	cudaMemcpy(arr, dev_arr, size * sizeof(float), cudaMemcpyDeviceToHost);	
 
  	for (int i = 0; i < size; i++) {
 		arr[i] = (size - i) * 1.5;
@@ -41,9 +28,6 @@ int main(void) {
 
  	printf("completed\n");
  	printf("%f\n", *max_val);
-
-	// cudaFree(dev_arr);
-	// cudaFree(dev_max_val);
 
 	free(arr);
 	free(max_val);
