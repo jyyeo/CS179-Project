f19a5d0091d719e9f00729deb909da648b1ad4a7
Yeo Jin Yan <jyyeo@caltech.edu>
2020-06-02 14:01:46 -0700

    reformat saxpy

--- test_saxpy/main.cpp
+++ test_saxpy/main.cpp
@@ -16,7 +16,7 @@ int main(void) {
 		x[i] = i + 0.5;
 		y[i] = i + 1.5;
 	}
-
+	printf("CPU: ");
 	for (int i = 0; i < size; i++) {
 		printf("%f  ", a * x[i] + y[i]);
 	}
@@ -24,6 +24,7 @@ int main(void) {
 
 	cudaSaxpy(a, x, y, size);
 
+	printf("GPU: ");
 	for (int i = 0; i < size; i++) {
 		printf("%f  ",y[i]);
 	}
