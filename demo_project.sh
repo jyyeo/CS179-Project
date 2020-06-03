b3a71039de62037866b0880fad261fa5427dbafa
Yeo Jin Yan <jyyeo@caltech.edu>
2020-06-02 17:32:58 -0700

    add demo script

---
+++ demo_project.sh
@@ -0,0 +1,20 @@
+./bins/cpu_run test_cases/test1.txt
+echo "cpu test1"
+./bins/cpu_run test_cases/test2.txt
+echo "cpu test2"
+./bins/cpu_run test_cases/test3.txt
+echo "cpu test3"
+./bins/cpu_run test_cases/test4.txt
+echo "cpu test4"
+./bins/cpu_run test_cases/test5.txt
+echo "cpu test5"
+./bins/gpu_run test_cases/test1.txt
+echo "gpu test1"
+./bins/gpu_run test_cases/test2.txt
+echo "gpu test2"
+./bins/gpu_run test_cases/test3.txt
+echo "gpu test3"
+./bins/gpu_run test_cases/test4.txt
+echo "gpu test4"
+./bins/gpu_run test_cases/test5.txt
+echo "gpu test5"
